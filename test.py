b39e10c7fc9e4f3decc1d5a08e7fb119844f0c12
maor2871 <ClassifiedAnonymouse@gmail.com>
2022-05-06 00:41:51 +0300

    Insert value to item's column added, option to upload a file to an item's column added as well. Need to add support for update window, and option to order the groups on the board.

--- test.py
+++ test.py
@@ -78,20 +78,20 @@ class WorkSpace():
 		"""
 		
 		# Get the data of the boards with graph-ql format.
-		boards_data = self.post_request(query='{ boards (limit:2) {id name groups{id title items{id name}} columns{id title type description}}}')
+		boards_data = self.post_request(query='{ boards (limit:1) {id name groups{id title} columns{id title type description} items{id name group{ id title } column_values{id text}} }}')
 		
 		# Get the list of boards, as a dictionary converted from json.
 		boards_json_list = boards_data['boards']
-		
+
 		# This list will eventual contain all the boards in the workspace.
 		self.boards = {}
 		
 		# Iterate over the boards.
 		for board in boards_json_list:
-
-			# Create the current board and append it the the boards list.
-			self.boards[board["name"]] = Board(ws=self, name=board['name'], board_id=board['id'], json_groups=board['groups'], json_columns=board['columns'])
 			
+			# Create the current board and append it the the boards list.
+			self.boards[board["name"]] = Board(ws=self, name=board['name'], board_id=board['id'], json_groups=board['groups'], json_columns=board['columns'], json_items=board['items'])
+	
 	def add_board(self, board):
 		"""
 			The function receives a board and adds it to the workspace.
@@ -105,7 +105,7 @@ class Board():
 		Represents a board in a work_space.
 	"""
 	
-	def __init__(self, ws, name, json_groups=None, json_columns=None, board_id=None):
+	def __init__(self, ws, name, json_groups=None, json_columns=None, json_items=None, board_id=None):
 		"""
 			Create an instance to an existing board.
 		"""
@@ -133,6 +133,9 @@ class Board():
 			
 			# Set the received groups as the groups of the board.
 			self.set_groups(json_groups)
+			
+			# Set the received items.
+			self.set_items(json_items)
 
 		# Create the board on monday too.
 		else:
@@ -171,8 +174,25 @@ class Board():
 		for group in json_groups:
 			
 			# Create and append the current group.
-			self.groups[group['title']] = Group(board=self, group_id=group['id'], title=group['title'], json_items=group['items'])
-	
+			self.groups[group['title']] = Group(board=self, group_id=group['id'], title=group['title'])
+			
+	def set_items(self, json_items):
+		"""
+			The function receives a json list of items. It creates and adds the items to their groups.
+		"""
+		
+		# Iterate over all the items in the board.
+		for item in json_items:
+			
+			# The group of the item.
+			item_group_title = item['group']['title']
+			
+			# Create the new item.
+			new_item = Item(group=item_group_title, name=item['name'], item_id=item['id'], json_columns_values=item['column_values'])
+			
+			# Add it to the group.
+			self.groups[item_group_title].add_item(new_item)
+		
 	def add_column(self, column):
 		"""
 			The function receives a column and adds it to the board.
@@ -193,7 +213,7 @@ class Group():
 		Represents a group of a board.
 	"""
 	
-	def __init__(self, board, title, json_items=None, group_id=None):
+	def __init__(self, board, title, group_id=None):
 		"""
 			Initialize the group.
 		"""
@@ -204,12 +224,12 @@ class Group():
 		# The title of the group.
 		self.title = title
 		
-		# The group already exists on monday, get its details.
-		if json_items:
+		# A list with all the items of the group.
+		self.items = {}
 		
-			# The items of the group.
-			self.items = self.set_items(json_items)
-			
+		# The group already exists on monday, get its details.
+		if group_id:
+
 			# The id of the group.
 			self.group_id = group_id
 			
@@ -218,9 +238,6 @@ class Group():
 
 			# Update it on monday.
 			self.group_id = self.board.work_space.post_request(query='mutation { create_group (board_id: ' + self.board.board_id + ', group_name: "' + self.title + '") { id } }')['create_group']['id']
-
-			# A list with all the items of the group.
-			self.items = {}
 		
 	def set_items(self, json_items):
 		"""
@@ -297,21 +314,22 @@ class Column():
 		if column_id:
 		
 			self.column_id = column_id
-			
+
 		# Create the column in monday.
 		else:
 
 			self.column_id = self.board.work_space.post_request(query='mutation{ create_column(board_id: ' + self.board.board_id + ', title:"' + self.title + '", description: "' + self.description + '", column_type:' + self.column_type + ') { id title description } }')['create_column']['id']
 		
-		
+
 class Item():
 	"""
 		Represents an item of a group.
 	"""
 	
-	def __init__(self, group, name, item_id=None):
+	def __init__(self, group, name, item_id=None, json_columns_values=None, columns_values=[]):
 		"""
 			Initialize the item.
+			Note: One of json_column_values or column_values must be specified. column_values is of the form: [(column title, value)].
 		"""
 		
 		# The group the item is within.
@@ -323,17 +341,63 @@ class Item():
 		# name of the item
 		self.name = name
 		
+		# The columns values of the item {column id: item's value}.
+		self.columns_values = {}
+
 		# The item already exists in monday.
 		if item_id:
-		
+
+			# Save its id.
 			self.item_id = item_id
-			
+
+			# Extract its column values.
+			self.set_columns(json_columns_values)
+
 		# Update the item in monday.
 		else:
+
+			# Convert the columns values to json format, as monday wants.
+			columns_values_json = '{' + ', '.join('\\\"' + self.group.board.columns[column_title].column_id + '\\\": \\\"' + value + '\\\"' for column_title, value in columns_values) + '}'
+
+			# Add the item to monday and save its id.
+			self.item_id = self.group.board.work_space.post_request(query='mutation {create_item (board_id: ' + self.group.board.board_id + ', group_id: "' + self.group.group_id + '", item_name: "' + self.name + '", column_values: "' + columns_values_json + '") { id } }')['create_item']['id']
+
+	def set_columns(self, json_columns_values):
+		"""
+			The item is already in monday. The function receives its columns values and saves them.
+		"""
 		
-			self.item_id = self.group.board.work_space.post_request(query='mutation {create_item (board_id: ' + self.group.board.board_id + ', group_id: "' + self.group.group_id + '", item_name: "' + self.name + '") { id } }')['create_item']['id']
+		# Iterate over the columns values.
+		for column_value in json_columns_values:
+			
+			# Save the column id and its value.
+			self.columns_values[column_value['id']] = column_value['text']
+			
+	def upload_files(self, column_title, files_paths):
+		"""
+			The function receives a list with files paths and a column and uploads the file to that column.
+		"""
+		for file_path in files_paths:
+		
+			# The query that makes the request to upload the file to the specific received column.
+			query = 'mutation ($file: File!) { add_file_to_column (file: $file, item_id: ' + self.item_id + ', column_id: "' + self.group.board.columns[column_title].column_id + '") {id }}'
 
+			# A list with all the files in the required format.
+			files=[('variables[file]', (file_path ,open(file_path, 'rb'), 'multipart/form-data'))]
+		
+			# Follow the format.
+			data = {'query': query}
+			
+			print("sending:", query)
 
+			# Send the post request and save the response as the received json string.
+			response_str = requests.post(url="https://api.monday.com/v2/file", headers={'Authorization': self.group.board.work_space.token}, data=data, files=files).text
+
+			# Convert the json string to the original object.
+			response = json.loads(response_str)
+			
+			print("response:", response)
+		
 """
 
 	Usage:
@@ -347,24 +411,27 @@ class Item():
 	# And you can create columns to the board.
 	my_board.add_column(Column(board=my_board, title="Date", description="When the row was added to the board", column_type="date"))
 	my_board.add_column(Column(board=my_board, title="Favourite color", description="the favourite color of the row", column_type="text"))
+	my_board.add_column(Column(board=my_board, title="Attached Files", description="", column_type="file"))
 	
-	# These columns are saved in my_board.columns. This is a dictionary of the form: {column title: column instance}
+	# These columns are saved in my_board.columns. This is a dictionary of the form: {column title: column instance}.
 	
 	# And you can create groups.
 	my_board.add_group(Group(board=my_board, title="An amazing group"))
 	my_board.add_group(Group(board=my_board, title="Another amazing group"))
 	
-	# These groups are saved in my_board.groups. This is a dictionary of the form: {group title: group instance}
+	# These groups are saved in my_board.groups. This is a dictionary of the form: {group title: group instance}.
 	
 	# Now you can add items to groups.
 	my_board.groups["An amazing group"].add_item(Item(group=my_board.groups["An amazing group"], name="Spectacular item 1"))
 	my_board.groups["An amazing group"].add_item(Item(group=my_board.groups["An amazing group"], name="Spectacular item 2"))
 	
 	# These items are saved in my_board.groups["An amazing group"].items. This is a dictionary of the form: {item name: item instance}.
+	
+	# You can upload a file to an item's column.
+	my_board.groups["An amazing group"].items["Spectacular item 1"].upload_files(column_title="Attached Files", files_paths=["path_to_local_file1", "path_to_local_file2"])
 
 """
-	
-	
+
 # Create the workspace.
 work_space = WorkSpace(token="eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE1ODI2MDM3MiwidWlkIjoyOTk1NzM5MSwiaWFkIjoiMjAyMi0wNC0yOVQyMTo0NzozNi4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE4NzU5MjIsInJnbiI6InVzZTEifQ.R7UplEfmGyfk1uPEr1A-UFNlcdCZ8VjfrGKl63WQYYo")
 
@@ -385,13 +452,18 @@ for course in ["Calculus", "Linear Algebra", "Combinatorics"]:
 	courses_board.add_group(Group(board=courses_board, title=course))
 
 # Add mails to each group.
-courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 1"))
-courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 2"))
-courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 3"))
+courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 1", columns_values=[("From", "Moshe"), ("Date", "2022-05-03")]))
+courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 2", columns_values=[("From", "Shalom"), ("Date", "2022-05-04")]))
+courses_board.groups["Calculus"].add_item(Item(group=courses_board.groups["Calculus"], name="mail 3", columns_values=[("From", "Yisaschar"), ("Date", "2022-05-07")]))
 courses_board.groups["Combinatorics"].add_item(Item(group=courses_board.groups["Combinatorics"], name="mail 1"))
-courses_board.groups["Linear Algebra"].add_item(Item(group=courses_board.groups["Linear Algebra"], name="mail 1"))
+courses_board.groups["Linear Algebra"].add_item(Item(group=courses_board.groups["Linear Algebra"], name="mail 2"))
 courses_board.groups["Linear Algebra"].add_item(Item(group=courses_board.groups["Linear Algebra"], name="mail 2"))
 
+# Add the attached files to the mails.
+courses_board.groups["Calculus"].items["mail 1"].upload_files(column_title="Attached Files", files_paths=['C:\python\MondayHackathon\hello world.txt', 'C:\python\MondayHackathon\Just another file.txt'])
+
+
+"""
 # --- Links Board ---
 
 # Create a new board for the links.
@@ -411,24 +483,12 @@ for period in ["Last Week", "Last Month", "Last 3 Months", "Last Year", "All Tim
 # Add mails to each group.
 links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 1"))
 links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 2"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 3"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 4"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 5"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 6"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 7"))
-links_board.groups["All Time"].add_item(Item(group=links_board.groups["All Time"], name="link 8"))
 links_board.groups["Last Year"].add_item(Item(group=links_board.groups["Last Year"], name="link 1"))
 links_board.groups["Last Year"].add_item(Item(group=links_board.groups["Last Year"], name="link 2"))
-links_board.groups["Last Year"].add_item(Item(group=links_board.groups["Last Year"], name="link 2"))
-links_board.groups["Last Year"].add_item(Item(group=links_board.groups["Last Year"], name="link 3"))
-links_board.groups["Last Year"].add_item(Item(group=links_board.groups["Last Year"], name="link 4"))
 links_board.groups["Last 3 Months"].add_item(Item(group=links_board.groups["Last 3 Months"], name="link 1"))
 links_board.groups["Last 3 Months"].add_item(Item(group=links_board.groups["Last 3 Months"], name="link 2"))
-links_board.groups["Last 3 Months"].add_item(Item(group=links_board.groups["Last 3 Months"], name="link 3"))
-links_board.groups["Last 3 Months"].add_item(Item(group=links_board.groups["Last 3 Months"], name="link 4"))
 links_board.groups["Last Month"].add_item(Item(group=links_board.groups["Last Month"], name="link 1"))
 links_board.groups["Last Month"].add_item(Item(group=links_board.groups["Last Month"], name="link 2"))
-links_board.groups["Last Month"].add_item(Item(group=links_board.groups["Last Month"], name="link 3"))
 links_board.groups["Last Week"].add_item(Item(group=links_board.groups["Last Week"], name="link 1"))
 links_board.groups["Last Week"].add_item(Item(group=links_board.groups["Last Week"], name="link 2"))
 
@@ -452,21 +512,10 @@ for period in ["Last Week", "Last Month", "Last 3 Months", "Last Year", "All Tim
 # Add mails to each group.
 attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 1"))
 attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 2"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 3"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 4"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 5"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 6"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 7"))
-attached_files_board.groups["All Time"].add_item(Item(group=attached_files_board.groups["All Time"], name="attached file 8"))
 attached_files_board.groups["Last Year"].add_item(Item(group=attached_files_board.groups["Last Year"], name="attached file 1"))
 attached_files_board.groups["Last Year"].add_item(Item(group=attached_files_board.groups["Last Year"], name="attached file 2"))
-attached_files_board.groups["Last Year"].add_item(Item(group=attached_files_board.groups["Last Year"], name="attached file 2"))
-attached_files_board.groups["Last Year"].add_item(Item(group=attached_files_board.groups["Last Year"], name="attached file 3"))
-attached_files_board.groups["Last Year"].add_item(Item(group=attached_files_board.groups["Last Year"], name="attached file 4"))
 attached_files_board.groups["Last 3 Months"].add_item(Item(group=attached_files_board.groups["Last 3 Months"], name="attached file 1"))
 attached_files_board.groups["Last 3 Months"].add_item(Item(group=attached_files_board.groups["Last 3 Months"], name="attached file 2"))
-attached_files_board.groups["Last 3 Months"].add_item(Item(group=attached_files_board.groups["Last 3 Months"], name="attached file 3"))
-attached_files_board.groups["Last 3 Months"].add_item(Item(group=attached_files_board.groups["Last 3 Months"], name="attached file 4"))
 attached_files_board.groups["Last Month"].add_item(Item(group=attached_files_board.groups["Last Month"], name="attached file 1"))
 attached_files_board.groups["Last Month"].add_item(Item(group=attached_files_board.groups["Last Month"], name="attached file 2"))
 attached_files_board.groups["Last Month"].add_item(Item(group=attached_files_board.groups["Last Month"], name="attached file 3"))
@@ -493,23 +542,13 @@ for period in ["Last Week", "Last Month", "Last 3 Months", "Last Year", "All Tim
 # Add mails to each group.
 secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  1"))
 secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  2"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  3"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  4"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  5"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  6"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  7"))
-secretariat_board.groups["All Time"].add_item(Item(group=secretariat_board.groups["All Time"], name="mail  8"))
 secretariat_board.groups["Last Year"].add_item(Item(group=secretariat_board.groups["Last Year"], name="mail  1"))
 secretariat_board.groups["Last Year"].add_item(Item(group=secretariat_board.groups["Last Year"], name="mail  2"))
-secretariat_board.groups["Last Year"].add_item(Item(group=secretariat_board.groups["Last Year"], name="mail  2"))
-secretariat_board.groups["Last Year"].add_item(Item(group=secretariat_board.groups["Last Year"], name="mail  3"))
-secretariat_board.groups["Last Year"].add_item(Item(group=secretariat_board.groups["Last Year"], name="mail  4"))
 secretariat_board.groups["Last 3 Months"].add_item(Item(group=secretariat_board.groups["Last 3 Months"], name="mail  1"))
 secretariat_board.groups["Last 3 Months"].add_item(Item(group=secretariat_board.groups["Last 3 Months"], name="mail  2"))
-secretariat_board.groups["Last 3 Months"].add_item(Item(group=secretariat_board.groups["Last 3 Months"], name="mail  3"))
-secretariat_board.groups["Last 3 Months"].add_item(Item(group=secretariat_board.groups["Last 3 Months"], name="mail  4"))
 secretariat_board.groups["Last Month"].add_item(Item(group=secretariat_board.groups["Last Month"], name="mail  1"))
 secretariat_board.groups["Last Month"].add_item(Item(group=secretariat_board.groups["Last Month"], name="mail  2"))
 secretariat_board.groups["Last Month"].add_item(Item(group=secretariat_board.groups["Last Month"], name="mail  3"))
 secretariat_board.groups["Last Week"].add_item(Item(group=secretariat_board.groups["Last Week"], name="mail  1"))
 secretariat_board.groups["Last Week"].add_item(Item(group=secretariat_board.groups["Last Week"], name="mail  2"))
+"""
